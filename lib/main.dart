857e7860886da8d29eca8f9d2d28965612cd09c9
Gravewalker666 <heshanandrews99@gmail.com>
2021-09-14 17:54:23 +0530

    Change the theme primary colour to white

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Super cool random infinite list",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
